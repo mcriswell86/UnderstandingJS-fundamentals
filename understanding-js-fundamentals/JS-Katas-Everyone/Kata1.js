04378b4a1a765c36ceb5b49ddaa80c758730bc43
mcriswell86 <mcriswell@icstars.org>
2025-10-22 14:28:37 -0500

    Update Kata1.js

--- understanding-js-fundamentals/JS-Katas-Everyone/Kata1.js
+++ understanding-js-fundamentals/JS-Katas-Everyone/Kata1.js
@@ -1,12 +1,12 @@
 // Kata 1: Print the first 10 even numbers
 // ** OBJECTIVE** Create your own loop using the following logic**
-// Commit 1: "scaffold loop structure"
 // Commit 2: "added even number condition"
 // Commit 3: "refactored variable names for clarity"
 
 
 for (let i = 5; i <=50; i++) {
+   if (i % 10 === 0) {
   console.log(i);
   
 }
-  
+}

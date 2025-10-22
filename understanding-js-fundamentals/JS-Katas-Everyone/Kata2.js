72cd1bd5fc379e13d73daec16b26a2d5a32c2574
mcriswell86 <mcriswell@icstars.org>
2025-10-22 16:41:37 -0500

    Update Kata2.js

--- understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
+++ understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
@@ -4,6 +4,18 @@
 // Commit 2: "added fizzbuzz logic"
 // Commit 3: "refactored with clear variable and comments"
 
+// Kata 2: FizzBuzz from 1 to 20
+// Commit 2: "added fizzbuzz logic"
+
 for (let i = 1; i <= 20; i++) {
-  console.log(i);
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
+

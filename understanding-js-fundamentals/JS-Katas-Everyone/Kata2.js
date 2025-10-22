b2d317d26abff990bbee60b618d033426972ce49
mcriswell86 <mcriswell@icstars.org>
2025-10-22 16:45:05 -0500

    Update Kata2.js

--- understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
+++ understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
@@ -7,15 +7,24 @@
 // Kata 2: FizzBuzz from 1 to 20
 // Commit 2: "added fizzbuzz logic"
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
+// Kata 2: FizzBuzz from 1 to 20
+// Commit 3: "refactored with clear variable and comments"
+
+for (let number = 1; number <= 20; number++) {
+  // Check if number is divisible by both 3 and 5
+  if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
+  } 
+  // Check if divisible by 3 only
+  else if (number % 3 === 0) {
     console.log("Fizz");
-  } else if (i % 5 === 0) {
+  } 
+  // Check if divisible by 5 only
+  else if (number % 5 === 0) {
     console.log("Buzz");
-  } else {
-    console.log(i);
+  } 
+  // Otherwise, just print the number
+  else {
+    console.log(number);
   }
 }
-
